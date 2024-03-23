3c2b1664e36be3bb6a2b69562061dde60b9787ce
Nicolas Klaue <klaue.nico@gmail.com>
2024-03-23 21:04:22 +0100

    FinalFront

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,64 @@ import "./App.css";
 import { Button } from "@mui/material";
 function App() {
   return (
-    <body className="App">
-        <div>
-          <div></div>
-          <div></div>
+    <body className="App h-screen w-screen p-14">
+      <div className="w-full h-full flex flex-row justify-evenly gap-10">
+        <div className="flex-col w-2/3 h-full gap-28 flex">
+          <div className="bg-slate-200 opacity-30 h-16 border-black border-4">
+              <textarea
+                id="EmailInput"
+                placeholder="Subject..."
+                className="w-full h-14 min-h-14 max-h-14 text-2xl font-semibold align-middle"
+              />
+          </div>
+          <div className="h-2/3 bg-slate-200 opacity-30 border-black border-4">
+            <textarea
+              id="EmailInput"
+              placeholder="Email body..."
+              className="w-full h-full max-h-full min-h-full text-lg font-semibold"
+            />
+          </div>
         </div>
 
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="flex flex-col gap-28">
+          <div className="flex flex-row">
+            <div className=" w-16 h-16 border-solid border-4 border-black bg-slate-200 opacity-30 text-center flex justify-center align-middle flex-col">
+              <p>1</p>
+            </div>
+            <div className=" w-16 h-16 border-solid border-4 border-black bg-slate-200 opacity-30 text-center flex justify-center align-middle flex-col">
+              <p>2</p>
+            </div>
+            <div className=" w-16 h-16 border-solid border-4 border-black bg-slate-200 opacity-30 text-center flex justify-center align-middle flex-col">
+              <p>3</p>
+            </div>
+            <div className=" w-16 h-16 border-solid border-4 border-black bg-slate-200 opacity-30 text-center flex justify-center align-middle flex-col">
+              <p>4</p>
+            </div>
+            <div className=" w-16 h-16 border-solid border-4 border-black bg-slate-200 opacity-30 text-center flex justify-center align-middle flex-col">
+              <p>5</p>
+            </div>
+          </div>
+          <div className="h-1/3">
+            <p
+              className="
+               text-gray-500
+               bg-slate-200
+               opacity-30
+               border-4
+               border-black
+               h-full
+               font-semibold"
+            >
+              Topics
+            </p>
+          </div>
+          <div>
+            <Button variant="contained" className="w-full">
+              Send email
+            </Button>
+          </div>
         </div>
-
-        <div>
-          <Button variant="contained">Send Email</Button>
-        </div>
+      </div>
     </body>
   );
 }
